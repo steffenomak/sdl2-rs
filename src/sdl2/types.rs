6bcb40c1d26fbe22b82b86fdd712d5979633267e
Stefan Risberg <steffenomak@gmail.com>
2013-10-29 21:37:01 +0100

    Fixed so that types got public stuff.

--- src/sdl2/types.rs
+++ src/sdl2/types.rs
@@ -1,7 +1,7 @@
 pub mod ffi {
     use std::libc::c_int;
 
-    type SDL_bool = c_int;
-    static SDL_TRUE: SDL_bool = 1;
-    static SDL_FALSE: SDL_bool = 0;
+    pub type SDL_bool = c_int;
+    pub static SDL_TRUE: SDL_bool = 1;
+    pub static SDL_FALSE: SDL_bool = 0;
 }
